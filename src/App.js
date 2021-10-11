b149ea178f658a4f21063f54179a138c42226c5a
Nurudeen Amedu <nurudeen.amedu@gmail.com>
2021-10-11 15:52:39 +0100

    style updates 4

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { useState } from 'react'
+import RiderProfile from './components/RiderProfile';
+
 import './App.css';
 
 import logo from './assets/talogo.png'
@@ -22,30 +25,41 @@ const bikes = [
 ]
 
 function App() {
+  const [list, setList] = useState(true)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} alt='logo' />
-        <table className="styled-table">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Bike</th>
-              <th>Registration</th>
-            </tr>
-          </thead>
-          <tbody>
-          {
-            bikes.map((item, index) => (
-              <tr key={index}>
-                <td>{index+1}</td>
-                <td>{item.model}</td>
-                <td>{item.reg}</td>
+      {list ? 
+        <div>
+          <img src={logo} alt='logo' />
+          <table className="styled-table">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Bike</th>
+                <th>Registration</th>
               </tr>
-            ))
-          }
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+            {
+              bikes.map((item, index) => (
+                <tr key={index} onClick={() => setList(false)}>
+                  <td>{index+1}</td>
+                  <td>{item.model}</td>
+                  <td>{item.reg}</td>
+                </tr>
+              ))
+            }
+            </tbody>
+          </table>
+        </div>
+        : 
+          <RiderProfile 
+            name="John Doe" 
+            location="Makarfi" 
+            id="TA-0001" 
+            setList={setList} />
+        }
       </header>
     </div>
   );
---
+++ src/components/RiderProfile.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import face from '../assets/face2.webp'
+import logo from '../assets/talogo.png'
+
+export default function RiderProfile({ name, id, location, setList }){
+
+    return(
+        <div className="profile">
+            <h1 onClick={() => setList(true)}>{'< Back'}</h1>
+            <div className="image-logo">
+                <img className="face" alt="face" src={face} />
+                <img className="logo" alt="logo" src={logo} />
+            </div>
+            <p>Name: {name}</p>
+            <p>Location: {location}</p>
+            <p>Rider ID: {id}</p>
+        </div>
+    )
+}

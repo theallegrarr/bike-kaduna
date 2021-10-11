e588b4cbc0d2790fe16dc939c3e2e536dabc06f2
Nurudeen Amedu <nurudeen.amedu@gmail.com>
2021-10-11 13:13:12 +0100

    add dummy bikes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+
+const bikes = [
+  { 
+    model: 'Honda',
+    reg: 'CB 829 ABJ',
+  },
+  { 
+    model: 'Honda',
+    reg: 'CB 429 ABJ',
+  },
+  { 
+    model: 'Honda',
+    reg: 'CB 569 ABJ',
+  },
+  { 
+    model: 'Honda',
+    reg: 'CB 729 ABJ',
+  },
+]
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          bikes.map((item, index) => (
+            <div key={index}>{item.model} | {item.reg}</div>
+          ))
+        }
       </header>
     </div>
   );
